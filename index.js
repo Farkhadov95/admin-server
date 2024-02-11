9f684cfc8f5aeb33c6fd20a9b3e294e81a74b750
Farkhadov95 <farkhadov950405@gmail.com>
2024-02-11 15:11:32 +0500

    Added CORS credentials

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ mongoose.connect(db)
 require('./startup/prod')(app);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.use('/api/regs', regs);
-app.use('/api/auth', auth)
+app.use('/api/auth', auth);
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
