46a51e548bcd04ea5d15997ce1f6ca2ddc2b3898
Farkhadov95 <farkhadov950405@gmail.com>
2024-02-15 17:35:29 +0500

    isolating email check

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,9 +7,12 @@ module.exports = async function auth(req, res, next) {
     const currentUserEmail = req.header('current-user-email');
     if (!token) return res.status(401).send('Access denied. No token provided.');
 
-    let currentUser = await Reg.findOne({ email: currentUserEmail });
-    if (!currentUser.isActive) return res.status(400).send('Access denied. This account is blocked');
-    if (!currentUser) return res.status(400).send('Access denied');
+    if (currentUserEmail) {
+        let currentUser = await Reg.findOne({ email: currentUserEmail });
+
+        if (!currentUser) return res.status(400).send('Access denied');
+        if (!currentUser.isActive) return res.status(400).send('Access denied. This account is blocked');
+    }
 
     try {
         const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
